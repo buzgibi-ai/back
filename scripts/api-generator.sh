ed77b55f5f0807ab4b8de8daa6d07a8eecdc8f18
Sergey Yakovlev <fclaw007@gmail.com>
2023-07-02 14:16:24 +0300

    docker ignore, api generator error  guard

--- scripts/api-generator.sh
+++ scripts/api-generator.sh
@@ -5,6 +5,27 @@ nm=$2
 package=$3
 dir=$4
 
+abort()
+{
+    echo >&2 '
+***************
+*** ABORTED ***
+***************
+'
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e
+
+echo >&2 '
+************
+*** START *** 
+************
+'
+
 openapi3-code-generator-exe \
   -f -o $dir/src/foreign/$nm \
   --package-name $package \
@@ -13,3 +34,13 @@ openapi3-code-generator-exe \
   --response-type-suffix $package \
   --generate-optional-empty-request-body=false $file && \
   python $dir/scripts/duplicate.py $dir/src/foreign/$nm/$package.cabal
+
+generate
+
+trap : 0
+
+echo >&2 '
+************
+*** DONE *** 
+************
+'
