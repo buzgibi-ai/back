c5bb8b166bbdcbd259431b6ada3962b55988b502
Sergey Yakovlev <fclaw007@gmail.com>
2023-07-03 00:02:53 +0300

    deploy

--- deploy/start.sh
+++ deploy/start.sh
@@ -13,14 +13,4 @@ cat <<EOT >> .env
   TAG=$tag
 EOT
 
-if [ $# -ge 1 ] && [ -n "$2" ]
-then 
-  composer=$2
-
-  if [ $composer == "yes" ]
-  then
-     exec docker-compose up
-  else
-     echo "arg must be yes" 
-  fi
-fi
+exec docker-compose up
