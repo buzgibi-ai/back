56df822ff13d15fbeaa034c329562c6bbdfce8b2
Sergey Yakovlev <fclaw007@gmail.com>
2023-07-03 22:24:18 +0300

    deploy

--- deploy/start.sh
+++ deploy/start.sh
@@ -17,7 +17,10 @@ cat <<EOT >> .env
   TAG=$tag
 EOT
 
-cp ~/ssl/buzgibi.crt ./deploy/nginx/ssl/buzgibi.crt
-cp ~/ssl/buzgibi.key ./deploy/nginx/ssl/buzgibi.key
+cp ~/ssl/front/buzgibi.crt ./deploy/nginx/ssl/front/buzgibi.crt
+cp ~/ssl/front/buzgibi.key ./deploy/nginx/ssl/front/buzgibi.key
+cp ~/ssl/back/buzgibi.crt ./deploy/nginx/ssl/back/buzgibi.crt
+cp ~/ssl/back/buzgibi.key ./deploy/nginx/ssl/back/buzgibi.key
+cp ~/ssl/global.pass ./deploy/nginx/ssl/global.pass
 
 exec docker-compose up -d
