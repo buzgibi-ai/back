20bd4fa8974177e309359c39bcb63428adf24eef
Sergey Yakovlev <fclaw007@gmail.com>
2023-07-22 20:11:53 +0300

    migration: open api transcription table

--- migration/index/version8.sql
+++ migration/index/version8.sql
@@ -1,6 +1,7 @@
-create table customer.phone_openai (
-    transcription_id bigserial not null,
+create table customer.phone_transcription (
+    transcription text not null,
+    survey_id bigserial not null,
     phone_id bigserial not null,
     constraint phone_openai__phone_id_fk foreign key (phone_id) references customer.survey_phones(id),
-    constraint phone_openai__transcription_id_fk foreign key (transcription_id) references storage.file(transcription_id),
-    constraint phone_openai__phone_transcription unique (phone_id, transcription_id));
+    constraint phone_openai__survey_id_fk foreign key (survey_id) references customer.survey(id),
+    constraint phone_openai__phone_survey unique (phone_id, survey_id));
