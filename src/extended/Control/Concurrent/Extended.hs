ab8ade8e4b3a60878fae6b608c5b8ee29f393096
Sergey Yakovlev <fclaw007@gmail.com>
2023-07-31 12:22:12 +0300

    concurrent

--- src/extended/Control/Concurrent/Extended.hs
+++ src/extended/Control/Concurrent/Extended.hs
@@ -1,10 +1,10 @@
-module Concurrent (throttle) where
+module Control.Concurrent.Extended (module Concurrent, throttle) where
 
-import Control.Concurrent
+import Control.Concurrent as Concurrent
 import Control.Concurrent.MSemN
-import qualified Control.Concurrent.Thread as Thread
 import Control.Exception
 import Control.Monad
+import qualified Control.Concurrent.Async as Async
 
 -- | Limit the number of @tasks@ started per second. @throttle@ will run all
 --   actions concurrently but only starting a certain number per second. It
@@ -15,10 +15,13 @@ throttle ::
   -- | the tasks to run concurrently but limited by TPS
   [IO a] ->
   -- | the tasks results
-  IO [a]
+  IO [Either SomeException a]
 throttle tps tasks = do
   sem <- new tps
-  let runTask task = fmap snd $ wait sem 1 >> Thread.forkIO task
-  let timeResetWorker = forever $ threadDelay 1000000 >> signalF sem (\i -> (tps - i, ()))
+  let runTask task = wait sem 1 >> (Async.async task >>= Async.waitCatch)
+  let timeResetWorker = 
+        forever $ do 
+          threadDelay 1000000
+          signalF sem (\i -> (tps - i, ()))
   let runAllTasks = mapM runTask tasks
-  mapM Thread.result =<< sequence =<< bracket (forkIO timeResetWorker) killThread (const runAllTasks)
+  bracket (forkIO timeResetWorker) killThread (const runAllTasks)
