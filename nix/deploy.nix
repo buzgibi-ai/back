e9524ae0f75bbaf7d1379e98b4b5d2a265529fb0
Sergey Yakovlev <fclaw007@gmail.com>
2023-08-28 18:54:19 +0300

    nix: add locale

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -1,6 +1,7 @@
 let
    pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.05.tar.gz") {};
 in
-pkgs.mkShell { 
-  buildInputs = [ pkgs.postgresql pkgs.lzma pkgs.zlib pkgs.imagemagick ];
+pkgs.mkShell {
+  LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive"; 
+  buildInputs = [ pkgs.postgresql pkgs.lzma pkgs.zlib pkgs.imagemagick pkgs.glibcLocales];
   }
